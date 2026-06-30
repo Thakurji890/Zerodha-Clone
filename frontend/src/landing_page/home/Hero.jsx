0d44512cacd96835a5c7c4a73b0f4f21a838592f
Thakurji890 <tha.rupesh456@gmail.com>
2026-06-30 12:01:10 +0530

    feat : adding hero section of the landing page

--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <>
+      <div className="conainer p-5">
+        <div className="row text-center">
+          <img src="image/homeHero.png" alt="Hero Image" className="mb-5" />
+          <h1 className="mt-5">Invest in everything</h1>
+          <p>
+            Online plateform to invest in stocks, derivatives, mutual funds, and
+            more
+          </p>
+          <button
+            className="p-2 btn btn-primary fs-5"
+            style={{ width: "10%", margin: "0 auto" }}
+          >
+            Signup Now
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Hero;
