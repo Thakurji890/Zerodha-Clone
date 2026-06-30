21c48d50510689060976bcb4e51c75a61b4a57b8
Thakurji890 <tha.rupesh456@gmail.com>
2026-06-30 14:14:52 +0530

    add : added prebuild react Arrow Icon component from Iconsax.io

---
+++ frontend/src/icons/Arrow.jsx
@@ -0,0 +1,28 @@
+export const Arrow = ({ size = 24, color = "#000000" }) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox={`0 0 24 24`}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14.4302 5.92969L20.5002 11.9997L14.4302 18.0697"
+        stroke={color}
+        stroke-width="1.5"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M3.5 12H20.33"
+        stroke={color}
+        stroke-width="1.5"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
