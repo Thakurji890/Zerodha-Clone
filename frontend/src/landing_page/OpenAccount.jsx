841f905778767bdaeb6dfa5ea819a49c4afdc187
Thakurji890 <tha.rupesh456@gmail.com>
2026-06-30 14:48:26 +0530

    style: remove unused React imports from layouts and home stubs

--- frontend/src/landing_page/OpenAccount.jsx
+++ frontend/src/landing_page/OpenAccount.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const OpenAccount = () => {
   return <div>OpenAccount</div>;
 };
--- frontend/src/landing_page/home/Pricing.jsx
+++ frontend/src/landing_page/home/Pricing.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { Arrow } from "../../icons/Arrow";
 
 const Pricing = () => {
-  return <div>Pricing</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-4">
+          <h1>Unbeatable pricing</h1>
+          <p>
+            We pioneered the concept of discount broking and price transparency
+            in India. Flat fees and no hidden charges.
+          </p>
+          <a href="">
+            See pricing <Arrow color="blue" />
+          </a>
+        </div>
+        <div className="col-2"></div>
+        <div className="col-6"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Pricing;
