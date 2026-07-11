3ea1f9cf45240d00880881e231858fd3135e9c9c
Thakurji890 <tha.rupesh456@gmail.com>
2026-07-11 20:36:59 +0530

    add : hero component added in products

--- frontend/src/landing_page/products/Hero.jsx
+++ frontend/src/landing_page/products/Hero.jsx
@@ -1,5 +1,23 @@
+import { Arrow } from "../../icons/Arrow";
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <>
+      <div className="container">
+        <div className="row text-center mt-5">
+          <h1>Zerodha Products</h1>
+          <p className="text-muted mt-2">
+            Sleek, modern, and intuitive trading platforms
+          </p>
+          <p className="text-muted mt-3" id="link">
+            Check out our{" "}
+            <a href="/investments">
+              investment offerings <Arrow color="currentColor" />
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Hero;
