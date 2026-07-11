375c6acdb0a330dee96a549c1b36ed767146d3ba
Thakurji890 <tha.rupesh456@gmail.com>
2026-07-11 22:11:44 +0530

    style: redesign product sections layout and typography

--- frontend/src/landing_page/products/LeftSection.jsx
+++ frontend/src/landing_page/products/LeftSection.jsx
@@ -1,5 +1,44 @@
-const LeftSection = () => {
-  return <div>LeftSection</div>;
+import { Arrow } from "../../icons/Arrow.jsx";
+import "../home/css/homePage.css";
+const LeftSection = ({
+  imageUrl,
+  productName,
+  productDescription,
+  tryDemo,
+  learnMore,
+  googlePlay,
+  appStore,
+}) => {
+  return (
+    <div className="container mt-5 mb-5 border-top">
+      <div className="row align-items-center p-5 mt-5">
+        <div className="col-12 col-md-7 p-3 p-md-5">
+          <img src={imageUrl} alt="product image" className="img-fluid" />
+        </div>
+        <div className="col-12 col-md-4 offset-md-1 p-3 p-md-5">
+          <h1 className="fs-2 mb-3">{productName}</h1>
+          <p className="text-muted">{productDescription}</p>
+          <div className="d-flex gap-4 mb-4 mt-4">
+            <a href={tryDemo} className="custom-link">
+              Try Demo <Arrow color="currentColor" />
+            </a>
+            <a href={learnMore} className="custom-link">
+              Learn More <Arrow color="currentColor" />
+            </a>
+          </div>
+
+          <div className="d-flex gap-3">
+            <a href={googlePlay}>
+              <img src="image/googlePlayBadge.svg" alt="play store image" />
+            </a>
+            <a href={appStore}>
+              <img src="image/appstoreBadge.svg" alt="app store image" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LeftSection;
--- frontend/src/landing_page/products/RightSection.jsx
+++ frontend/src/landing_page/products/RightSection.jsx
@@ -1,5 +1,31 @@
-const RightSection = () => {
-  return <div>RightSection</div>;
+import { Arrow } from "../../icons/Arrow";
+import "../home/css/homePage.css";
+
+const RightSection = ({
+  imageUrl,
+  productName,
+  productDescription,
+  learnMore,
+  link,
+}) => {
+  return (
+    <div className="container mt-5 mb-5 border-top">
+      <div className="row align-items-center p-5 mt-5">
+        <div className="col-12 col-md-4 p-3 p-md-5">
+          <h1 className="fs-2 mb-3">{productName}</h1>
+          <p className="text-muted">{productDescription}</p>
+          <div className="mt-4">
+            <a href={learnMore} className="custom-link">
+              {link} <Arrow color="currentColor" />
+            </a>
+          </div>
+        </div>
+        <div className="col-12 col-md-7 offset-md-1 p-3 p-md-5">
+          <img src={imageUrl} alt="product image" className="img-fluid" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RightSection;
