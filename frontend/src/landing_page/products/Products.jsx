d2c8c3364a13dc36e1fa0607dc2587327daf3f98
Thakurji890 <tha.rupesh456@gmail.com>
2026-07-12 00:37:55 +0530

    feat : left and right component added

--- frontend/src/landing_page/products/Products.jsx
+++ frontend/src/landing_page/products/Products.jsx
@@ -9,8 +9,54 @@ const Products = () => {
   return (
     <>
       <Hero />
-      <LeftSection />
-      <RightSection />
+      <LeftSection
+        imageUrl="image/kite.png"
+        productName="Kite"
+        productDescription="Our ultra-fast flagship trading platform with streaming market data, 
+        advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices."
+        tryDemo="https://kite-demo.zerodha.com"
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <RightSection
+        imageUrl="image/console.png"
+        productName="Console"
+        productDescription="The central dashboard for your Zerodha account. 
+        Gain insights into your trades and investments with in-depth reports and visualisations."
+        learnMore=""
+        link="Learn More"
+      />
+
+      <LeftSection
+        imageUrl="image/coin.png"
+        productName="Coin"
+        productDescription="Buy direct mutual funds online, commission-free, delivered directly to your Demat account. 
+        Enjoy the investment experience on your Android and iOS devices."
+        tryDemo="https://coin.zerodha.com/"
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <RightSection
+        imageUrl="image/kiteconnect.png"
+        productName="Kite Connect API"
+        productDescription="Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. 
+        If you are a startup, build your investment app and showcase it to our clientbase."
+        learnMore=""
+        link="Kite Connect"
+      />
+
+      <LeftSection
+        imageUrl="image/varsity.png"
+        productName="Varsity mobile"
+        productDescription="An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations.
+         Content is broken down into bite-size cards to help you learn on the go."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
       <Universe />
     </>
   );
