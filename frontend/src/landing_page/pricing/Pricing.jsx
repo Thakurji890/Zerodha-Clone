4bc0eefc984d94ad2412189491628d6a270b3543
Thakurji890 <tha.rupesh456@gmail.com>
2026-07-12 17:29:55 +0530

    style: restructure Pricing page layout and add OpenAccount section

--- frontend/src/landing_page/pricing/Pricing.jsx
+++ frontend/src/landing_page/pricing/Pricing.jsx
@@ -3,12 +3,14 @@ import Hero from "./Hero";
 
 import Navbar from "../Navbar";
 import Footer from "../Footer";
+import OpenAccount from "../OpenAccount";
 
 const Pricing = () => {
   return (
     <>
-      <Brokerage />
       <Hero />
+      <OpenAccount />
+      <Brokerage />
     </>
   );
 };
