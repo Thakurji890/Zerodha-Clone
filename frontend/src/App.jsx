0895813c1c0ea923a4b557ab535a3bd7d7aa4c5f
Thakurji890 <tha.rupesh456@gmail.com>
2026-07-12 17:29:55 +0530

    fix: correct import path for PricingPage in App routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./landing_page/signup/Signup";
 import AboutPage from "./landing_page/about/About";
 import ProductPage from "./landing_page/products/Products";
-import PricingPage from "./landing_page/home/Pricing";
+import PricingPage from "./landing_page/pricing/Pricing";
 import SupportPage from "./landing_page/support/SupportPage";
 import Navbar from "./landing_page/Navbar";
 import Footer from "./landing_page/Footer";
