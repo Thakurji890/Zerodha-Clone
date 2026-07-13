cc649c272df4cdc31f703cc80f10ced419367b43
Thakurji890 <tha.rupesh456@gmail.com>
2026-07-13 20:12:45 +0530

    feat: initialize clean dashboard frontend project

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,16 @@
+import Home from "./components/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
